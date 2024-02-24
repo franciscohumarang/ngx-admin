66e2c32361c097d54eabe8e1ffa06775d2077cc0
Francisco Humarang <fmhumarang@gmail.com>
2024-02-24 17:35:15 +0800

    UI changes and update package

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -25,15 +25,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     {
       value: 'dark',
       name: 'Dark',
-    },
-    {
-      value: 'cosmic',
-      name: 'Cosmic',
-    },
-    {
-      value: 'corporate',
-      name: 'Corporate',
-    },
+    }
   ];
 
   currentTheme = 'default';
--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,16 +2,16 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
+    title: 'Home',
+    icon: 'home-outline',
     link: '/pages/dashboard',
     home: true,
-  },
+  },/*
   {
-    title: 'IoT Dashboard',
+    title: 'Home,
     icon: 'home-outline',
     link: '/pages/iot-dashboard',
-  },
+  },*/
   {
     title: 'FEATURES',
     group: true,
